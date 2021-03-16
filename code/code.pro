37f23171081917450751c500999ad9f254022466
Batiana226 <mbatiana.nacro@gmail.com>
2021-03-16 11:55:05 +0100

    ajout du code main ainsi qu des codes barres utilisés pour tester

--- code/code.pro
+++ code/code.pro
@@ -1,3 +1,4 @@
+include(vendors/src/QZXing.pri)
 QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
@@ -29,3 +30,6 @@ FORMS += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    ../../image.qrc
--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
-
+#include "QZXing"
+#include "QZXing.h"
 #include <QApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+        QImage imageToDecode(":/Lecteur_CodeBarre/code/index3.png");
+        QZXing decoder;
+            //mandatory settings
+        decoder.setDecoder(QZXing:: DecoderFormat_QR_CODE | QZXing:: DecoderFormat_EAN_13 );
+
+            //optional settings
+            decoder.setSourceFilterType(QZXing::SourceFilter_ImageNormal | QZXing::SourceFilter_ImageInverted);
+            //decoder.setSourceFilterType(QZXing::SourceFilter_ImageNormal);
+            decoder.setTryHarderBehaviour(QZXing::TryHarderBehaviour_ThoroughScanning | QZXing::TryHarderBehaviour_Rotate);
+
+            //trigger decode
+        QString result = decoder.decodeImage(imageToDecode);
+        cout << result.toStdString()<<endl;
+
 }
